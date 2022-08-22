c7d1b2c8091bf197ae66c8a46f477b800668a723
dfish19 <dfish19@yahoo.com>
2022-08-22 11:16:40 -0500

    initial

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,7 @@
+﻿using StaticExercise;
+
+var f = TempConverter.FahrenheitToCelsius(98.6);
+var c = TempConverter.CelsiusToFahrenheit(23.5);
+Console.WriteLine(f);
+Console.WriteLine(c);
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double num)
+        {
+            return num;
+        }
+        public static double CelsiusToFahrenheit(double temp)
+        {
+           return temp;
+        }
+    }
+}
